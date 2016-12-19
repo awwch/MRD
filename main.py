03eba2d810c5f59c0bd7f87d0ea01e32ecce84e0
awwch <drolls4ever@gmail.com>
2016-12-19 08:48:51 +0300

    fine html

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 DEFAULT_PORT = 5000
 ADDITIVE_FOR_UID = 1000
 
@@ -39,39 +38,6 @@ def tutorial():
 @app.route('/index.html')
 def index():
     return render_template('index.html')
-    
-def open_dict(path):
-    f = open(path,'r',encoding = 'utf-8')
-    _dict = f.read().split('<superEntry>')
-    f.close()
-    return _dict
-    
-def tag_cleaner(art):
-    fine_elements = []
-    elements = art.split('\n')
-    for element in elements:
-        if element != '':
-            m = re.match('<.+>',element)
-            if m != None:
-                element = element.strip(m.group())
-        fine_elements.append(element)
-    fine_art = '\n'.join(fine_elements)
-    return fine_art
-    
-def fine_dict(_dict):
-    all_words = []
-    for art in _dict:        
-        if art == '':
-            _dict.remove(art)
-        else:
-            art = tag_cleaner(art)
-            elements = art.split('\n')
-            word = elements[1].strip('<orth>, ').lower()
-            all_words.append({word:art})
-    return all_words
-    
-_dict = open_dict('bts+tei.txt')
-all_words = fine_dict(_dict)
 @app.route('/index.html', methods=['POST'])
 def search():
     key = request.form["key_word"]
@@ -79,12 +45,36 @@ def search():
     i = 0
     for word in all_words:  
         if key == list(word.keys())[0]:
-            result = list(word.values())[0].strip('"')
+            result = list(word.values())[0]
         else:
             i += 1        
     if i >= len(all_words):
         result = 'Not found'    
     return render_template('/index.html', key=key, result=result)
+    
+def open_dict(path):
+    f = open(path,'r',encoding = 'utf-8')
+    _dict = f.read().split('<div class="superEntry">')
+    f.close()
+    return _dict
+    
+def fine_dict(_dict):
+    all_words = []
+    for art in _dict:      
+        if art == '':
+            _dict.remove(art)
+        else:
+            m = re.search('<div class="headword">\n\t*<p>.+',art)
+            if m != None:
+                word = m.group().lower()
+                word = re.sub(r'\<[^>]*\>', '', word).strip()
+                all_words.append({word:art})
+    return all_words
+
+_dict = open_dict('C:\\Users\\Ania\\Desktop\\mrd-site\\MRD no bs\\gs_html.txt')        
+all_words = fine_dict(_dict)
 
 if __name__ == '__main__':
     app.run(port=getuid() + ADDITIVE_FOR_UID, debug=True)
+
+ 
